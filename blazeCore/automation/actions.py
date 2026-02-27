60dea158442573cbcdb36be60a930833a5224fbe
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-02-27 19:56:51 -0300

    Atualizar o actions.py

--- blazeCore/automation/actions.py
+++ blazeCore/automation/actions.py
@@ -1,9 +1,20 @@
+from automation.cec_control import TVController
 import webbrowser
-from printer.bambu_monitor import get_printer_status
+
+tv = TVController()
 
 def execute(intent):
+
     if intent["intent"] == "open_youtube":
+        tv.power_on()
+        tv.set_active_source()
         webbrowser.open("https://youtube.com")
 
-    if intent["intent"] == "printer_status":
-        return get_printer_status()
+    if intent["intent"] == "tv_off":
+        tv.standby()
+
+    if intent["intent"] == "volume_up":
+        tv.volume_up()
+
+    if intent["intent"] == "volume_down":
+        tv.volume_down()
